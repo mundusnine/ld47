6f7ab1dfc15217640155743b9390756ccfa7a7bd
mundusnine <mundusnine@gmail.com>
2020-10-27 17:05:22 -0400

    Update foundsdk and Main.hx

--- Sources/Main.hx
+++ Sources/Main.hx
@@ -4,11 +4,6 @@ import found.Found;
 
 class Main {
 
-	#if editor
-	public static var sha = utilities.BuildMacros.sha().substr(1, 7);
-	public static var date = utilities.BuildMacros.date().split(" ")[0];
-	#end
-
 	public static function main(){
 		//To see the whole FoundConfig, go have a look at Found.hx in foundry2d library
 		Found.setup({app:Project, title:'Foundry2d'});
